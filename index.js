85b9dfc9f9cccdfa76eea4d978c52a1ec147e957
Moztanku <Mostankpl@gmail.com>
2021-06-28 17:26:14 +0200

    stuff

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
+const customers = require('./routes/customers');
 const genres = require('./routes/genres');
 const main = require('./routes/main');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mongo-exercises',{useUnifiedTopology: true,useNewUrlParser: true})
+    .then(  ()=>console.log('Connected to the DB...')    )
+    .catch( err=>console.log('Couldn\'t connect to the DB: ',err.message)   );
 
 app.use(express.json());
 
 app.use('/',main);
 app.use('/api/genres',genres);
+app.use('/api/customers',customers)
 
 const port = process.env.PORT || 3000;
 app.listen(port,'localhost',()=>{
--- routes/genres.js
+++ routes/genres.js
@@ -1,45 +1,83 @@
 const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
+const mongoose = require('mongoose');
 
-const genres = [
-    {id:1,value:'action'},
-    {id:2,value:'horror'},
-    {id:3,value:'fantasy'}
-];
-
-router.get('/',(req,res)=>{
-    res.send(genres);
-});
-router.get('/:id',(req,res)=>{
-    let genre = genres.find(c=>c.id===parseInt(req.params.id))
-    res.send(genre?genre:`Genre with and id of ${req.params.id} doesn't exist.`);
-});
-router.post('/',(req,res)=>{
-    const {error} = validate(req.body);
-    if(error) return res.status(400).send(error);
-    let genre = {
-        id: genres[genres.length-1].id+1,
-        value:req.body.value
+const Genres = new mongoose.model('Genre',{
+    id:{
+        type: Number,
+        required: true
+    },
+    value:{
+        type: String,
+        minlength: 3,
+        maxlength: 12,
+        required: true
     }
-    genres.push(genre);
-    res.send(genres);
 });
-router.put('/:id',(req,res)=>{    
-    let genre = genres.find(c=>c.id===parseInt(req.params.id));
-    if(!genre) return res.status(404).send(`Genre with and id of ${req.params.id} doesn't exist.`);
 
+async function getGenresCount(){
+    const result = await Genres.findOne({}).sort({id:-1}).select({id:1,_id:0}).lean();
+    return result.id;
+};
+getGenresCount().then(res=>genresCount=res);
+
+router.get('/',async (req,res)=>{
+    try{
+        let genres = await Genres.find({}).lean();
+        res.send(genres);
+    }
+    catch(ex){
+        console.log(ex.message);
+    }
+});
+router.get('/:id',async (req,res)=>{
+    try{    
+        let result = await Genres.findOne({id:Number.parseInt(req.params.id)}).lean();
+        res.send(result?result:'Genre with a given id doesn\'t exist.');
+    }
+    catch(ex){
+        console.log(ex.message);
+        res.send(ex.message);
+    }
+});
+router.post('/',async(req,res)=>{
     const {error} = validate(req.body);
     if(error) return res.status(400).send(error);
-
-    genre.value = req.body.value;
-    res.send(genre);
+    try{
+        let genre = new Genres({
+            id: ++genresCount,
+            value: req.body.value
+        });
+        let result = await genre.save();
+        res.send(result.toObject());
+    }
+    catch(ex){
+        console.log(ex.message);
+        res.send(ex.message);
+    }
+});
+router.put('/:id',async(req,res)=>{
+    const {error} = validate(req.body);
+    if(error) return res.status(400).send(error);
+    try{
+        let result = await Genres.findOneAndUpdate({id:Number.parseInt(req.params.id)},{value:req.body.value},{useFindAndModify: false});
+        res.send(result?result.toObject():'Genre with a given id doesn\'t exist.');
+    }
+    catch(ex){
+        console.log(ex.message);
+        res.send(ex.message);
+    }
 })
-router.delete('/:id',(req,res)=>{
-    let genre = genres.find(c=>c.id===parseInt(req.params.id));
-    if(!genre) return res.status(404).send(`Genre with and id of ${req.params.id} doesn't exist.`);
-    let index = genres.indexOf(genre);
-    return res.send(genres.splice(index,1));
+router.delete('/:id',async(req,res)=>{
+    try{
+        let result = await Genres.findOneAndDelete({id:Number.parseInt(req.params.id)},{useFindAndModify: false});
+        res.send(result?result.toObject():'Genre with a given id doesn\'t exist.');
+    }
+    catch(ex){
+        console.log(ex.message);
+        res.send(ex.message);
+    }
 })
 function validate(data){
     const schema = Joi.object({
